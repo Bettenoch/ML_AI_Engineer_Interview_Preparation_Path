3854540c9457cec4733109c217285c77cc312795
Bettenoch <bettenoch254@gmail.com>
2025-06-03 10:58:57 +0300

    PRACTICE: algos

---
+++ day1-Advanced_data_structures_for_ml/test.py
@@ -0,0 +1,43 @@
+from collections import defaultdict
+class HarshTree:
+    def __init__(self, bucket):
+        self.bucket = bucket
+        self.table = [[] for _ in range(bucket)]
+        
+    def _harsh(self, key):
+        return hash(key) % self.bucket
+
+    def put( self, key, value):
+        index = self._harsh(key)
+        
+        for i, (k, v) in enumerate(self.table[index]):
+            if index == k:
+                self.table[index][i] = (key, value)
+                return
+            self.table[index].append(key, value)
+    def get(self, key):
+        index = self._harsh(key)
+        bucket = self.table[index]
+        
+        for k, v in bucket:
+            if key == k:
+                return v
+        raise KeyError(f"Key '{key}' not found")
+    
+    def delete(self, key):
+        index = self._harsh(key)
+        bucket = self.table[index]
+        
+        for i, (k,v) in bucket:
+            if key == k:
+                del(bucket[i])
+        raise KeyError(f"Key '{key}' not found")
+    
+dy = defaultdict(list)
+
+pairs = [("a", 1), ("b", 1), ("c", 1), ("a", 2), ("c", 4),]
+
+for k, v in pairs:
+    dy[k].append(v)
+    
+print (dy)
